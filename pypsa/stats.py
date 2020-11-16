0f6fa0664de744ed2c1d958784406e0d47c173bc
Lukas Barth <lukas.barth@kit.edu>
2020-11-16 15:46:53 +0100

    Suggestion for cost calculation

--- pypsa/stats.py
+++ pypsa/stats.py
@@ -18,15 +18,82 @@ idx = pd.IndexSlice
 # Network summary
 # =============================================================================
 
-opt_name = {"Store": "e", "Line" : "s", "Transformer" : "s"}
+opt_name = {"Store": "e", "Line": "s", "Transformer": "s"}
+
+
+def calculate_operational_cost(n):
+    # For each component incurring marginal cost, list:
+    # Component, amount, marginal cost
+    marginal_cost_drivers = (('generators', 'p', 'marginal_cost'),
+                             ('storage_units', 'p', 'marginal_cost'),
+                             ('stores', 'p', 'marginal_cost'))
+    marginal_cost_per_driver = {}
+    total_marginal_cost = 0.0
+
+    for (component_name, amount_field, cost_field) in marginal_cost_drivers:
+        if not hasattr(n, component_name):
+            continue
+
+        amounts = getattr(n, f"{component_name}_t")[amount_field]
+        # Filter out negative values (e.g., charging a storage unit)
+        amounts[amounts < 0] = 0
+
+        # TODO According to the docs, marginal costs may be series. Are these then in *_t?
+        costs = getattr(n, component_name)[cost_field]
+
+        # DataFrame. X Axis: Unit / Y Axis: Time. Contains costs per unit at each time step
+        full_costs = amounts.mul(costs)
+
+        cost_per_unit = full_costs.sum()
+        this_driver_cost = cost_per_unit.sum()
+        marginal_cost_per_driver[f"{component_name}__{amount_field}"] = this_driver_cost
+        total_marginal_cost += this_driver_cost
+
+    # Compute cost for startup / shutdown of generators
+    status_change = n.generators_t['status'].diff()
+    startups = (status_change == 1).astype(int).sum()
+    shutdowns = (status_change == -1).astype(int).sum()
+    # Filter costs down to generators present in startup/shutdown series
+    startup_cost_series = n.generators['start_up_cost'].filter(
+        items=startups.index)
+    shutdown_cost_series = n.generators['shut_down_cost'].filter(
+        items=shutdowns.index)
+    startup_cost = startup_cost_series.dot(startups)
+    shutdown_cost = shutdown_cost_series.dot(shutdowns)
+    commitment_cost = startup_cost + shutdown_cost
+
+    return (total_marginal_cost, marginal_cost_per_driver, commitment_cost)
+
+
+def calculate_investment_cost(n):
+    # List for every cost driver: component, base amount, extended amount, cost field
+    cost_drivers = (('generators', 'p_nom', 'p_nom_opt', 'capital_cost'),
+                    ('lines', 's_nom', 's_nom_opt', 'capital_cost'),
+                    ('storage_units', 'p_nom', 'p_nom_opt', 'capital_cost'),
+                    ('stores', 'e_nom', 'e_nom_opt', 'capital_cost'),
+                    ('links', 'p_nom', 'p_nom_opt', 'capital_cost'),
+                    ('transformers', 's_nom', 's_nom_opt', 'capital_cost'))
+    investment_cost_per_driver = {}
+    total_investment_cost = 0.0
+
+    for (component_name, base_amount_field, extended_amount_field, cost_field) in cost_drivers:
+        component = getattr(n, component_name)
+        extension = component[extended_amount_field] - \
+            component[base_amount_field]
+        # Filter out negatives, which could theoretically exist?
+        extension[extension < 0] = 0
+        extension_cost = extension.dot(component[cost_field])
+        investment_cost_per_driver[f"{component_name}__{base_amount_field}"] = extension_cost
+        total_investment_cost += extension_cost
+
+    return (total_investment_cost, investment_cost_per_driver)
+
 
 def calculate_costs(n):
-    raise NotImplementedError
-    mc = {}
-    for c in n.iterate_comonents():
-        if 'marginal_cost' in c.df:
+    total_investment_cost, _ = calculate_investment_cost(n)
+    total_marginal_cost, _, commitment_cost = calculate_operational_cost(n)
 
-            mc[c] = c.df @ c.pnl['p']
+    return total_investment_cost + total_marginal_cost + commitment_cost
 
 
 def calculate_curtailment(n):
@@ -57,7 +124,8 @@ def describe_storage_unit_contraints(n):
     """
     sus = n.storage_units
     sus_i = sus.index
-    if sus_i.empty: return
+    if sus_i.empty:
+        return
     sns = n.snapshots
     c = 'StorageUnit'
     pnl = n.pnl(c)
@@ -72,22 +140,21 @@ def describe_storage_unit_contraints(n):
     spill = eh[pnl.spill.columns] * pnl.spill
 
     description['Spillage Limit'] = pd.Series({'min':
-                                (inflow[spill.columns] - spill).min().min()})
+                                               (inflow[spill.columns] - spill).min().min()})
 
     if 'p_store' in pnl:
         soc = pnl.state_of_charge
 
-        store = store_eff * eh * pnl.p_store#.clip(upper=0)
-        dispatch = 1/dispatch_eff * eh * pnl.p_dispatch#(lower=0)
+        store = store_eff * eh * pnl.p_store  # .clip(upper=0)
+        dispatch = 1/dispatch_eff * eh * pnl.p_dispatch  # (lower=0)
         start = soc.iloc[-1].where(sus.cyclic_state_of_charge,
                                    sus.state_of_charge_initial)
         previous_soc = stand_eff * soc.shift().fillna(start)
 
-
         reconstructed = (previous_soc.add(store, fill_value=0)
-                        .add(inflow, fill_value=0)
-                        .add(-dispatch, fill_value=0)
-                        .add(-spill, fill_value=0))
+                         .add(inflow, fill_value=0)
+                         .add(-dispatch, fill_value=0)
+                         .add(-spill, fill_value=0))
         description['SOC Balance StorageUnit'] = ((reconstructed - soc)
                                                   .unstack().describe())
     else:
@@ -101,11 +168,12 @@ def describe_nodal_balance_constraint(n):
     Helper function to double check whether network flow is balanced
     """
     network_injection = pd.concat(
-            [n.pnl(c)[f'p{inout}'].rename(columns=n.df(c)[f'bus{inout}'])
-            for inout in (0, 1) for c in ('Line', 'Transformer')], axis=1)\
-            .groupby(level=0, axis=1).sum()
+        [n.pnl(c)[f'p{inout}'].rename(columns=n.df(c)[f'bus{inout}'])
+         for inout in (0, 1) for c in ('Line', 'Transformer')], axis=1)\
+        .groupby(level=0, axis=1).sum()
     return (n.buses_t.p - network_injection).unstack().describe()\
-            .to_frame('Nodal Balance Constr.')
+        .to_frame('Nodal Balance Constr.')
+
 
 def describe_upper_dispatch_constraints(n):
     '''
@@ -114,11 +182,12 @@ def describe_upper_dispatch_constraints(n):
     description = {}
     key = ' Upper Limit'
     for c, attr in nominal_attrs.items():
-        dispatch_attr = 'p0' if c in ['Line', 'Transformer', 'Link'] else attr[0]
+        dispatch_attr = 'p0' if c in [
+            'Line', 'Transformer', 'Link'] else attr[0]
         description[c + key] = pd.Series({'min':
-                               (n.df(c)[attr + '_opt'] *
-                               get_as_dense(n, c, attr[0] + '_max_pu') -
-                               n.pnl(c)[dispatch_attr]).min().min()})
+                                          (n.df(c)[attr + '_opt'] *
+                                           get_as_dense(n, c, attr[0] + '_max_pu') -
+                                           n.pnl(c)[dispatch_attr]).min().min()})
     return pd.concat(description, axis=1)
 
 
@@ -129,15 +198,15 @@ def describe_lower_dispatch_constraints(n):
         if c in ['Line', 'Transformer', 'Link']:
             dispatch_attr = 'p0'
             description[c] = pd.Series({'min':
-                              (n.df(c)[attr + '_opt'] *
-                              get_as_dense(n, c, attr[0] + '_max_pu') +
-                              n.pnl(c)[dispatch_attr]).min().min()})
+                                        (n.df(c)[attr + '_opt'] *
+                                         get_as_dense(n, c, attr[0] + '_max_pu') +
+                                         n.pnl(c)[dispatch_attr]).min().min()})
         else:
             dispatch_attr = attr[0]
             description[c + key] = pd.Series({'min':
-                                   (-n.df(c)[attr + '_opt'] *
-                                   get_as_dense(n, c, attr[0] + '_min_pu') +
-                                   n.pnl(c)[dispatch_attr]).min().min()})
+                                              (-n.df(c)[attr + '_opt'] *
+                                               get_as_dense(n, c, attr[0] + '_min_pu') +
+                                               n.pnl(c)[dispatch_attr]).min().min()})
     return pd.concat(description, axis=1)
 
 
@@ -147,7 +216,8 @@ def describe_store_contraints(n):
     """
     stores = n.stores
     stores_i = stores.index
-    if stores_i.empty: return
+    if stores_i.empty:
+        return
     sns = n.snapshots
     c = 'Store'
     pnl = n.pnl(c)
@@ -159,11 +229,12 @@ def describe_store_contraints(n):
     previous_e = stand_eff * pnl.e.shift().fillna(start)
 
     return (previous_e - pnl.p - pnl.e).unstack().describe()\
-            .to_frame('SOC Balance Store')
+        .to_frame('SOC Balance Store')
 
 
 def describe_cycle_constraints(n):
-    weightings = n.lines.x_pu_eff.where(n.lines.carrier == 'AC', n.lines.r_pu_eff)
+    weightings = n.lines.x_pu_eff.where(
+        n.lines.carrier == 'AC', n.lines.r_pu_eff)
 
     def cycle_flow(sub):
         C = pd.DataFrame(sub.C.todense(), index=sub.lines_i())
@@ -174,7 +245,6 @@ def describe_cycle_constraints(n):
 
     return pd.concat([cycle_flow(sub) for sub in n.sub_networks.obj], axis=0)\
              .unstack().describe().to_frame('Cycle Constr.')
-
 
 
 def constraint_stats(n, round_digit=1e-30):
@@ -189,7 +259,8 @@ def constraint_stats(n, round_digit=1e-30):
                       describe_nodal_balance_constraint(n),
                       describe_lower_dispatch_constraints(n),
                       describe_upper_dispatch_constraints(n)],
-                   axis=1, sort=False)
+                     axis=1, sort=False)
+
 
 def check_constraints(n, tol=1e-3):
     """
@@ -213,7 +284,3 @@ def check_constraints(n, tol=1e-3):
     condition = stats.T[['Min', 'Max']].query('Min < -@tol | Max > @tol').T
     assert condition.empty, (f'The following constraint(s) are exceeding the '
                              f'given tolerance of {tol}: \n{condition}')
-
-
-
-
